b73a27e6acccfba16426620fba3f262d0b683227
Charly <Charly@Charly-PC>
2014-07-02 16:46:14 -0400

    Add missing Tests classes

---
+++ tests/ca/etsmtl/log430/lab3/tests/ColumnSortFilterTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package ca.etsmtl.log430.lab3.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Charly
+ *
+ */
+public class ColumnSortFilterTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
